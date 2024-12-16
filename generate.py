0c7533b8e351351a4dccde7931697014c0d6ac70
pokutuna <mail@pokutuna.com>
2024-12-17 01:20:29 +0900

    例作る → 回答再生成 でのモデル再ロード回数を減らす

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import re
 
 import ollama
@@ -22,25 +21,39 @@ def run(
     outfile: str,
     ollama_host: str | None,
 ) -> None:
-    logging.info("Start generation")
+    print("Start generation")
     client = ollama.Client(host=ollama_host)
 
+    tasks = df_tasks.to_dicts()
     outputs = []
 
     # まず一通り生成
-    logging.info(f"step1. Generate outputs with {model=}")
-    for row in tqdm(df_tasks.to_dicts()):
+    print(f"step1. Generate outputs with {model=}")
+    for row in tqdm(tasks):
         output = generate(client, model, row["input"])
         outputs.append(output)
     write_outputs(df_tasks, outputs, outfile)
 
     # ことわざ・慣用句に関する問題はサブの力を借りる
-    logging.info("step2. Improve outputs for Japanese tasks")
-    for i, row in enumerate(tqdm(df_tasks.to_dicts())):
+    print("step2. Improve outputs for Japanese tasks")
+    generated_examples: dict[int, list[str]] = {}
+
+    print("step2-1. Generate examples")
+    # モデルの読み込み回数を減らすため先に全体舐めて例を作る
+    for i, row in enumerate(tqdm(tasks)):
         if re.search(r"(?:ことわざ|諺|慣用句)", row["input"]):
-            examples = [generate(client, sub_model, row["input"]) for _ in range(3)]
-            prompt = f"{row['input']}\n\n### 参考出力\n" + "\n".join(examples)
-            outputs[i] = generate(client, model, prompt)
+            es = [generate(client, sub_model, row["input"]) for _ in range(3)]
+            generated_examples[i] = es
+
+    print("step2-2. Generate outputs with examples")
+    # 例を使ってメインモデルで再生成
+    for i, row in enumerate(tqdm(tasks)):
+        if i not in generated_examples:
+            continue
+        es = generated_examples[i]
+        examples = ["- " + re.sub(r"\n+", " ", e).strip() for e in es]
+        prompt = f"{row['input'].strip()}\n\n### 参考出力\n" + "\n".join(examples)
+        outputs[i] = generate(client, model, prompt)
     write_outputs(df_tasks, outputs, outfile)
 
 
@@ -69,17 +82,23 @@ def write_outputs(df_tasks: pl.DataFrame, outputs: list[str], path: str) -> None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tasks", type=str, required=True)
-    parser.add_argument("--outfile", type=str, default="./output.jsonl")
-    parser.add_argument("--ollama-host", type=str)
+    parser.add_argument("--tasks", type=str, required=True, help="タスクファイル")
+    parser.add_argument("--outfile", type=str, default="./output.jsonl", help="出力先")
+    parser.add_argument(
+        "--ollama-host",
+        type=str,
+        help="ollama サーバのホスト, 省略時はデフォルト",
+    )
     parser.add_argument(
         "--model",
         type=str,
+        help="主に利用するモデル",
         default="hf.co/pokutuna/llm2024-competition:gemma2-9b-v11-Q6_K.gguf",
     )
     parser.add_argument(
         "--sub-model",
         type=str,
+        help="サブで利用するモデル、model に比べことわざ・慣用句に強いもの",
         default="hf.co/pokutuna/llm2024-competition:llm-jp-3-13b-v2-Q6_K.gguf",
     )
     args = parser.parse_args()
@@ -87,7 +106,7 @@ if __name__ == "__main__":
     run(
         df_tasks=read_as_df(args.tasks),
         model=args.model,
-        sub_model=args.ollama_host,
+        sub_model=args.sub_model,
         outfile=args.outfile,
         ollama_host=args.ollama_host,
     )
